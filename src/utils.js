a1a23921114de9ae46cff655d1f4bcad5f400a3c
guillejf <maildeguille@gmail.com>
2023-06-03 14:21:49 -0300

    v4

--- src/utils.js
+++ src/utils.js
@@ -32,7 +32,7 @@ import { connect } from "mongoose";
 export async function connectMongo() {
   try {
     await connect(
-      "mongodb+srv://guillermofergnani:d3IUa8A4QOAZkoQa@guille-cluster.pzfq0ua.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://guillermofergnani:@guille-cluster.pzfq0ua.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("plug to mongo!");
   } catch (e) {
